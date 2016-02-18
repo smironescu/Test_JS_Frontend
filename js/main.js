f67ce71cdf8655cbd6ea976f4408318c826f63b1
smironescu <sergio.mironescu@marfeel.com>
2016-02-18 20:33:23 +0100

    Updated css - mixin

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ require(["charts/circleRevenue",
         "charts/circleImpresions",
         "charts/circleVisits"], function(circleRevenue, circleImpresions, circleVisits){
 
-    new circleRevenue(47, "#007007", "#00e32f");
-    new circleImpresions(70, "#005369", "#00ceed");
-    new circleVisits(70, "#e94c00", "#ffc800");
+    new circleRevenue(40, "#007007", "#00e32f");
+    new circleImpresions(60, "#005369", "#00ceed");
+    new circleVisits(20, "#e94c00", "#ffc800");
 
 });
